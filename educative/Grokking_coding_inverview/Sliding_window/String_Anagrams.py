58c44fe48e364c33565b4b68cee9077f6040ffc3
tvoronenko <tetiana.voronenko@gmail.com>
2022-01-14 19:58:31 -0800

    Update

---
+++ educative/Grokking_coding_inverview/Sliding_window/String_Anagrams.py
@@ -0,0 +1,16 @@
+'''
+Given a string and a pattern, find all anagrams of the pattern in the given string.
+
+'''
+
+def find_string_anagrams(str, pattern):
+  result_indexes = []
+  # TODO: Write your code here
+  return result_indexes
+
+def main():
+    print(find_string_anagrams("ppqp", "pq"))  # [1,2]
+    print(find_string_anagrams("abbcabc", "abc"))  # [2,3,4]
+
+
+main()
